6b436c71455b349d24b960e56b3d1c1072b5fab7
dennyabrain <denny.george90@gmail.com>
2015-04-18 19:22:31 -0400

    Click Logic and Computer Move implemented. Computer AI pending

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 window.onload = function(){
+	//data sctructures for the game
+	var game=[[0,0,0],[0,0,0],[0,0,0]];
+
+	function printArray(){
+		console.log("======");
+		console.log('- ' + game[0][0] + ' - ' + game[0][1] + ' - ' + game[0][2]);
+		console.log('- ' + game[1][0] + ' - ' + game[1][1] + ' - ' + game[1][2]);
+		console.log('- ' + game[2][0] + ' - ' + game[2][1] + ' - ' + game[2][2]);
+		console.log("======");
+	}
+
+	printArray();
+
+	//Click handlers
 	for(i=0;i<3;i++){
 		for(j=0;j<3;j++){
 			var id = ''+i+''+j;
@@ -7,8 +21,40 @@ window.onload = function(){
 			document.getElementById(id).style.height='100px';
 			document.getElementById(id).addEventListener('click', function(event){
 				console.log(event.toElement.id);
+				drawX(event.toElement.id);
 			})
 		}
 	}	
+
+	function drawX(id){
+		var i=id[0]
+		var j=id[1]
+		//console.log('index ' + i + ' ' + j);
+		
+				if(game[i][j]==0){ //check to see if the box is empty
+					game[i][j]=1;
+					document.getElementById(id).innerHTML='X';
+					computerMove();
+					//return;
+				}
+		//document.getElementById(id).innerHTML='X';
+	}
+
+	function computerMove(){
+		var valueChanged = 0;
+		for(i=0;i<3;i++){
+			for(j=0;j<3;j++){
+				if(game[i][j]==0){//if the box is empty draw a 0
+					game[i][j]=2;
+					var id = ''+i+''+j;
+					document.getElementById(id).innerHTML='0';
+					printArray();
+					return;
+				}
+			}
+		}
+	}
+
 }
+
 
