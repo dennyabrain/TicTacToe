a1f2c563ef6dca7856f5754a605a8f9623da55af
staff <staff@172-16-245-136.dynapool.nyu.edu>
2015-04-20 15:51:03 -0400

    intelligent move implemented. Blocking Win Pending

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 window.onload = function(){
 	//data sctructures for the game
 	var game=[[0,0,0],[0,0,0],[0,0,0]];
+	var PoLR=[0,0,0,0,0,0,0,0]; //Path of least resistance
 
+	//console.log(getWinner());
+	getPoLR();
+	console.log(PoLR);
 	function printArray(){
 		console.log("======");
 		console.log('- ' + game[0][0] + ' - ' + game[0][1] + ' - ' + game[0][2]);
@@ -10,7 +14,7 @@ window.onload = function(){
 		console.log("======");
 	}
 
-	printArray();
+	//printArray();
 
 	//Click handlers
 	for(i=0;i<3;i++){
@@ -20,7 +24,7 @@ window.onload = function(){
 			document.getElementById(id).style.width='100%';
 			document.getElementById(id).style.height='100px';
 			document.getElementById(id).addEventListener('click', function(event){
-				console.log(event.toElement.id);
+				//console.log(event.toElement.id);
 				drawX(event.toElement.id);
 			})
 		}
@@ -35,26 +39,186 @@ window.onload = function(){
 					game[i][j]=1;
 					document.getElementById(id).innerHTML='X';
 					computerMove();
+					//console.log(getWinner());
+					if(getWinner()==1 || getWinner()==2){
+						alert('Your winner is Player ' + getWinner());
+						location.reload();
+					}
+					
 					//return;
 				}
 		//document.getElementById(id).innerHTML='X';
 	}
 
 	function computerMove(){
-		var valueChanged = 0;
+		getPoLR();
+		console.log(PoLR);
+		var move = intelligentMove();
+		insertO(move);
+	}
+
+	function checkWinner(){
 		for(i=0;i<3;i++){
 			for(j=0;j<3;j++){
-				if(game[i][j]==0){//if the box is empty draw a 0
-					game[i][j]=2;
-					var id = ''+i+''+j;
-					document.getElementById(id).innerHTML='0';
-					printArray();
-					return;
+				if(game[i][j]==0){
+
 				}
 			}
 		}
 	}
 
+	function getWinner(){
+		//console.log('computing winner');
+		for(i=0;i<3;i++){
+			if(game[i][0] && game[i][0]==game[i][1] && game[i][1]==game[i][2]){
+				return game[i][0];
+			}
+		}
+
+		for(i=0;i<3;i++){
+			if(game[0][i] && game[0][i]==game[1][i] && game[1][i]==game[2][i]){
+				return game[i][0];
+			}
+		}
+	}
+
+	function getPoLR(){
+		PoLR=[0,0,0,0,0,0,0,0];
+		for(i=0;i<3;i++){
+			if(game[i][0]==0) PoLR[i]++;
+			if(game[i][1]==0) PoLR[i]++;
+			if(game[i][2]==0) PoLR[i]++;
+		}
+		for(i=0;i<3;i++){
+			if(game[0][i]==0) PoLR[i+3]++;
+			if(game[1][i]==0) PoLR[i+3]++;
+			if(game[2][i]==0) PoLR[i+3]++;
+		}
+		if(game[0][0]==0) PoLR[6]++;
+		if(game[1][1]==0) PoLR[6]++;
+		if(game[2][2]==0) PoLR[6]++;
+
+		if(game[0][2]==0) PoLR[7]++;
+		if(game[1][1]==0) PoLR[7]++;
+		if(game[2][0]==0) PoLR[7]++;
+	}
+
+	function intelligentMove(){
+		var val=Math.max.apply(Math,PoLR);
+		//console.log(val);
+		var index=PoLR.indexOf(val);
+		//console.log(index);
+		return index;
+	}
+
+	function insertO(indPoLR){
+		switch(indPoLR){
+			case 0:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[0][ind]==0){
+						game[0][ind]=2;
+						var i = 0;
+						var j = ind;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 1:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[1][ind]==0){
+						game[1][ind]=2;
+						var i = 1;
+						var j = ind;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 2:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[2][ind]==0){
+						game[2][ind]=2;
+						var i = 2;
+						var j = ind;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 3:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[ind][0]==0){
+						game[ind][0]=2;
+						var i = ind;
+						var j = 0;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 4:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[ind][1]==0){
+						game[ind][1]=2;
+						var i = ind;
+						var j = 1;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 5:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[ind][2]==0){
+						game[ind][2]=2;
+						var i = ind;
+						var j = 2;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 6:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[ind][ind]==0){
+						game[ind][ind]=2;
+						var i = ind;
+						var j = ind;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+			case 7:
+				while(1){
+					var ind = Math.floor(Math.random()*3);
+					if(game[2-ind][ind]==0){
+						game[2-ind][ind]=2;
+						var i = 2-ind;
+						var j = ind;
+						var id=''+i+''+j;
+						document.getElementById(id).innerHTML='O';
+						break;
+					}
+				}
+				break;
+		}
+	}
 }
 
 
